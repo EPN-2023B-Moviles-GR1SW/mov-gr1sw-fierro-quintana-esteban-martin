163d599f630979d3322de47875f26a48c5ae8ab1
pc_martin <130690477+martinfq@users.noreply.github.com>
2023-12-07 00:11:28 -0500

    Clase 6 y 7

--- 02-Android/app/src/main/java/com/example/app1_gr1sw/MainActivity.kt
+++ 02-Android/app/src/main/java/com/example/app1_gr1sw/MainActivity.kt
@@ -3,41 +3,12 @@ package com.example.app1_gr1sw
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.app1_gr1sw.ui.theme.App1gr1swTheme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            App1gr1swTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    App1gr1swTheme {
-        Greeting("Android")
-    }
-}
